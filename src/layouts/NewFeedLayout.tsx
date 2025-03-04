a83b96c9708c6831794f7fd330f309d10d51e41b
pyaephyowin <pyaephyowin.dev@gmail.com>
2025-03-05 02:00:54 +0630

    add sample layout

--- src/layouts/NewFeedLayout.tsx
+++ src/layouts/NewFeedLayout.tsx
@@ -1,9 +1,25 @@
+import { Container, Paper, Text } from "@mantine/core";
 import { Outlet } from "react-router";
 
 export function NewFeedLayout() {
   return (
     <div>
-      <Outlet />
+      <Paper
+        component="header"
+        display="flex"
+        px="20px"
+        bg="primary"
+        c="#fff"
+        radius={0}
+      >
+        <Text size="32px" fw={700} component="h1" w="8ch" py="20px">
+          University Magazine
+        </Text>
+      </Paper>
+
+      <Container size="xl">
+        <Outlet />
+      </Container>
     </div>
-  )
+  );
 }
