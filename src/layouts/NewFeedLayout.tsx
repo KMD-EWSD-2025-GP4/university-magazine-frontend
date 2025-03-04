4f87eb4367c630ba5ef5f6ede5970508b2e9939b
pyaephyowin <pyaephyowin.dev@gmail.com>
2025-03-05 02:06:58 +0630

    add tmp logout btn

--- src/layouts/NewFeedLayout.tsx
+++ src/layouts/NewFeedLayout.tsx
@@ -1,7 +1,10 @@
-import { Container, Paper, Text } from "@mantine/core";
-import { Outlet } from "react-router";
+import { handleLogout } from "@/utils/auth";
+import { Button, Container, Flex, Paper, Text } from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
+import { Outlet, useNavigate } from "react-router";
 
 export function NewFeedLayout() {
+  const navigate = useNavigate();
   return (
     <div>
       <Paper
@@ -12,9 +15,25 @@ export function NewFeedLayout() {
         c="#fff"
         radius={0}
       >
-        <Text size="32px" fw={700} component="h1" w="8ch" py="20px">
-          University Magazine
-        </Text>
+        <Flex w="100%" align="center">
+          <Text size="32px" fw={700} component="h1" w="8ch" py="20px">
+            University Magazine
+          </Text>
+
+          <Button
+            onClick={() => {
+              handleLogout(() => {
+                navigate("/login");
+                showNotification({
+                  title: "Logout Success!",
+                  message: "You have successfully logged out.",
+                });
+              });
+            }}
+          >
+            Logout
+          </Button>
+        </Flex>
       </Paper>
 
       <Container size="xl">
