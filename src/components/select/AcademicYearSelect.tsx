13854821d9bd3415d277f1b8e926c655ea01e332
pyaephyowin <pyaephyowin.dev@gmail.com>
2025-04-16 20:46:30 +0630

    update date format

--- src/components/select/AcademicYearSelect.tsx
+++ src/components/select/AcademicYearSelect.tsx
@@ -1,13 +1,15 @@
 import { BaseSelect } from "./BaseSelect";
 import { SelectProps } from "@mantine/core";
 import { useGetAcademicYears } from "@/routes/academic-years/queries";
-import { formatDate } from "@/utils/dates";
+import { formatAcademicYear } from "@/utils/dates";
 
 export function AcademicYearSelect(props: SelectProps) {
   const { data, isLoading } = useGetAcademicYears();
 
   const normalizedData = data?.map((ay) => ({
-    label: `${formatDate(ay.startDate)} - ${formatDate(ay.endDate)}`,
+    label: `${formatAcademicYear(ay.startDate)} - ${formatAcademicYear(
+      ay.endDate
+    )}`,
     value: ay.id,
   }));
 
